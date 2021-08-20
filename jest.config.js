d89138706e6a39be2db1590539e278336a0f6578
Hellshi <hcslimaa@gmail.com>
2021-08-20 19:50:00 -0300

    chore: changed eslint rules

--- jest.config.js
+++ jest.config.js
@@ -10,8 +10,6 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
-  transform: {
-    '.+\\.ts$': 'ts-jest',
-  },
+  transform: { '.+\\.ts$': 'ts-jest' },
 
 };
