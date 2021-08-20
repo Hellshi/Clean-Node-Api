c489fd5875b1989569adfba71807c9e01f2ac3a9
Hellshi <hcslimaa@gmail.com>
2021-08-20 19:49:20 -0300

    test: singUp class returns 400 when no email is provided
    n

--- src/presentation/controller/singup.spec.ts
+++ src/presentation/controller/singup.spec.ts
@@ -17,4 +17,21 @@ describe('', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new Error('missing param: name'));
   });
+
+  test('Should return 400 when no email is provided ', () => {
+    const sut = new SingUpController();
+
+    const httpResquest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirm: 'any_password',
+      },
+    };
+
+    const httpResponse = sut.handle(httpResquest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('missing param: email'));
+  });
 });
