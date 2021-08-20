c733e1d31e18ceba05c16a1267a0a1315ee777b5
Hellshi <hcslimaa@gmail.com>
2021-08-20 20:29:13 -0300

    refactor: moved duplicated code to a helper method

--- src/presentation/controller/singup.ts
+++ src/presentation/controller/singup.ts
@@ -1,21 +1,16 @@
 import { MissingParamError } from './errors/missing-param-error';
+import { badRequest } from './helpers/http-helpers';
 import { httpResponse, httpResquest } from './protocols/http';
 
 /* eslint-disable class-methods-use-this */
 export class SingUpController {
   handle(httpResquest: httpResquest): httpResponse {
     if (!httpResquest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('name'),
-      };
+      return badRequest(new MissingParamError('name'));
     }
 
     if (!httpResquest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('email'),
-      };
+      return badRequest(new MissingParamError('email'));
     }
   }
 }
