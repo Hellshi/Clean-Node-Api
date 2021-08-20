fef8ae4fcca125816964d2faf301695a6149b62d
Hellshi <hcslimaa@gmail.com>
2021-08-20 20:34:45 -0300

    refactor: simplify code

--- src/presentation/controller/singup.ts
+++ src/presentation/controller/singup.ts
@@ -5,12 +5,12 @@ import { httpResponse, httpResquest } from './protocols/http';
 /* eslint-disable class-methods-use-this */
 export class SingUpController {
   handle(httpResquest: httpResquest): httpResponse {
-    if (!httpResquest.body.name) {
-      return badRequest(new MissingParamError('name'));
-    }
+    const requiredFields = ['name', 'email'];
 
-    if (!httpResquest.body.email) {
-      return badRequest(new MissingParamError('email'));
+    for (const field of requiredFields) {
+      if (!httpResquest.body[field]) {
+        return badRequest(new MissingParamError(field));
+      }
     }
   }
 }
