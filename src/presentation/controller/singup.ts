197f2ee7264ea836d174e94f561ac7764a70dc2f
Hellshi <hcslimaa@gmail.com>
2021-08-20 19:30:30 -0300

    feat: ensure httpResponse.body to be "missing param: name" when no name is provided

--- src/presentation/controller/singup.ts
+++ src/presentation/controller/singup.ts
@@ -1,6 +1,9 @@
 /* eslint-disable class-methods-use-this */
 export class SingUpController {
   handle(httpResquest: any): any {
-    return { statusCode: 400 };
+    return {
+      statusCode: 400,
+      body: new Error('missing param: name'),
+    };
   }
 }
