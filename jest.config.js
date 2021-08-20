a990089299a60256397b82c05306637229a0e7fd
Hellshi <hcslimaa@gmail.com>
2021-08-20 18:08:03 -0300

    chore: added and configurated JEST

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable max-len */
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  roots: ['<rootDir/src>'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+
+};
