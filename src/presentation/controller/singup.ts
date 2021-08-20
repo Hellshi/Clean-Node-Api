a10406bca378a208da08ce9abd7e32a91e656f5c
Hellshi <hcslimaa@gmail.com>
2021-08-20 19:48:03 -0300

    feat: ensure singup returns 400 when no email is provided

--- src/presentation/controller/singup.ts
+++ src/presentation/controller/singup.ts
@@ -1,9 +1,18 @@
 /* eslint-disable class-methods-use-this */
 export class SingUpController {
   handle(httpResquest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('missing param: name'),
-    };
+    if (!httpResquest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('missing param: name'),
+      };
+    }
+
+    if (!httpResquest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('missing param: email'),
+      };
+    }
   }
 }
