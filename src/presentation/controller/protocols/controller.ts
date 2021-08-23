15c3d77d8d1d2285bef9d1f9b33d0390ba5d77ef
Hellshi <hcslimaa@gmail.com>
2021-08-23 09:43:04 -0300

    feat: ensure singUp controller returns 400 if an invalid email is provided

--- src/presentation/controller/protocols/controller.ts
+++ src/presentation/controller/protocols/controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { httpResponse, httpResquest } from './http';
 
 export interface Controller {
--- src/presentation/controller/singup.ts
+++ src/presentation/controller/singup.ts
@@ -1,10 +1,17 @@
+import { InvalidParamError } from './errors/invalid-param-error copy';
 import { MissingParamError } from './errors/missing-param-error';
 import { badRequest } from './helpers/http-helpers';
 import { Controller } from './protocols/controller';
+import { EmailValidator } from './protocols/email-validator';
 import { httpResponse, httpResquest } from './protocols/http';
 
-/* eslint-disable class-methods-use-this */
 export class SingUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator;
+  }
+
   handle(httpResquest: httpResquest): httpResponse {
     const requiredFields = ['name', 'email', 'passwordConfirm', 'password'];
 
@@ -12,6 +19,12 @@ export class SingUpController implements Controller {
       if (!httpResquest.body[field]) {
         return badRequest(new MissingParamError(field));
       }
+    }
+
+    const isValid = this.emailValidator.isValid(httpResquest.body.email);
+
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'));
     }
   }
 }
