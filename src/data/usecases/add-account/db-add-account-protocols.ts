94fc920c8363ac2bd5c04b94c388a3698e81367c
Hellshi <hcslimaa@gmail.com>
2021-08-25 13:34:58 -0300

    feat: ensures DbAddAccount calls AddAccountRepository with correct values

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,3 +1,4 @@
 export * from '../../../domain/usecases';
 export * from '../../../domain/models/account';
 export * from '../../protocols/envrypter';
+export * from '../../protocols/add-account-repository';
