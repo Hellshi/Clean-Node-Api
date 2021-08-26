e75f1de57242de06820f6214421837cb3a1a9dc4
Hellshi <hcslimaa@gmail.com>
2021-08-26 09:01:44 -0300

    chore: added Jest config to MongoDb

--- jest.config.js
+++ jest.config.js
@@ -10,5 +10,6 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
+  preset: '@shelf/jest-mongodb',
   transform: { '.+\\.ts$': 'ts-jest' },
 };
--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,10 @@
 import bcrypt from 'bcrypt';
 import { BcryptAdapter } from './bcrypt-adapter';
 
+const salt = 12;
+
+const makeSut = () => new BcryptAdapter(salt);
+
 jest.mock('bcrypt', () => ({
   async hash(): Promise<string> {
     return new Promise((resolve) => resolve('hash'));
@@ -9,17 +13,23 @@ jest.mock('bcrypt', () => ({
 
 describe('Bcrypt adapter', () => {
   test('should call Encrypt with correct value', async () => {
-    const salt = 12;
-    const sut = new BcryptAdapter(salt);
+    const sut = makeSut();
     const hashSpy = jest.spyOn(bcrypt, 'hash');
     await sut.encrypt('any_value');
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt);
   });
 
   test('should return hash on sucess', async () => {
-    const salt = 12;
-    const sut = new BcryptAdapter(salt);
+    const sut = makeSut();
     const hash = await sut.encrypt('any_value');
     expect(hash).toBe('hash');
+  });
+
+  test('Throws when Encrypt throws', async () => {
+    const sut = makeSut();
+    // @ts-ignore: Unreachable code error
+    jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())));
+    const promise = sut.encrypt('any_value');
+    await expect(promise).rejects.toThrow();
   });
 });
