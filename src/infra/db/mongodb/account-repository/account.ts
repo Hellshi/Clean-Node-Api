1a1856eac68da2a185bf66da3bf5ecd189baff37
Hellshi <hcslimaa@gmail.com>
2021-08-26 11:36:47 -0300

    feat: ensure mongoAccountReository returns an account on Sucess

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,14 +1,14 @@
-import { AccountModel } from "../../../../domain/models/account";
-import { AddAccount, AddAccountModel } from "../../../../domain/usecases";
-import { mongoHelpers } from "../helpers/mongo-helpers";
+import { AccountModel } from '../../../../domain/models/account';
+import { AddAccount, AddAccountModel } from '../../../../domain/usecases';
+import { mongoHelpers } from '../helpers/mongo-helpers';
 
 export class AccountMongoRepository implements AddAccount {
-   async add(accountData: AddAccountModel): Promise<AccountModel> {
-     const accountCollection = mongoHelpers.getCollection('accounts')
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = mongoHelpers.getCollection('accounts');
 
-     const result = await accountCollection.insertOne(accountData);
-     const account = result.insertedId
-      // @ts-ignore: Unreachable code error
-     return result
-   }
+    const result = await accountCollection.insertOne(accountData);
+    const account = result.ops[0];
+    const { _id, ...accountWithoutId } = account;
+    return { ...accountWithoutId, id: _id };
+  }
 }
