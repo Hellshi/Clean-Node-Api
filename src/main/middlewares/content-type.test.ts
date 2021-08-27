6c4bf5a7ec72cf1fbd93ef65748ff57b3bc34c79
Hellshi <hcslimaa@gmail.com>
2021-08-27 12:09:51 -0300

    fix: fixed bug on json middleware test
    It was trying to read the post response when it should read the get

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -2,11 +2,10 @@ import request from 'supertest';
 import app from '../config/app';
 
 describe('Content Type Middleware', () => {
-  test('should return default content type as json', async () => {
-    app.post('/test_content_type', (req, res) => {
+  test('Should return default content type as json', async () => {
+    app.get('/test_content_type', (req, res) => {
       res.send('');
     });
-
     await request(app)
       .get('/test_content_type')
       .expect('content-type', /json/);
