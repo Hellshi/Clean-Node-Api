1e256eadca6110ff4b9faeca0288a4af9b2533b4
Hellshi <hcslimaa@gmail.com>
2021-08-30 10:48:19 -0300

    feat: connect to mongodb before app.listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,8 @@
-import app from './config/app';
+import { mongoHelpers } from '../infra/db/mongodb/helpers/mongo-helpers';
+import env from './config/env';
 
-app.listen(3333, () => console.log('O Adminstrador está online no camarote http://localhost:3333'));
+mongoHelpers.connect(env.mongoUrl).then(async () => {
+  const app = (await import('./config/app')).default;
+
+  app.listen(env.PORT, () => console.log(`Running on http://localhost:${env.PORT}`));
+}).catch(console.error);
