020c409d6c945492561e53205cf8ce029835a8b5
Hellshi <hcslimaa@gmail.com>
2021-08-30 10:59:18 -0300

    fix: fixed MongoDb url acession

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,4 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://hell:621251@cluster0.ym2g4.mongodb.net/clean-node-api?retryWrites=true&w=majority',
   PORT: 5050 || 3333,
 };
