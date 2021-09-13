1e511cd632b9c51ee06193ab97402aa4bec48c1d
Hellshi <hcslimaa@gmail.com>
2021-09-13 09:35:51 -0300

    feat: ensure JwtAdapter returns a token on success

--- src/infra/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/jwt-adapter/jwt-adapter.spec.ts
@@ -1,11 +1,24 @@
 import jwt from 'jsonwebtoken';
 import { JwtAdapter } from './jwt-adapter';
 
+jest.mock('jsonwebtoken', () => ({
+  async sign(): Promise<string> {
+    return 'any_token';
+  },
+}));
+
 describe('JWT adapter', () => {
   test('should call sing with correct values', async () => {
     const sut = new JwtAdapter('secret');
     const JWTSpy = jest.spyOn(jwt, 'sign');
-    sut.encrypt('any_id');
+    await sut.encrypt('any_id');
     expect(JWTSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret');
+  });
+
+  test('should return acess token on sucess', async () => {
+    const sut = new JwtAdapter('secret');
+
+    const accessToken = await sut.encrypt('any_id');
+    expect(accessToken).toBe('any_token');
   });
 });
